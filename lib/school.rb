08f7d3b6923fefed7c755fa400e39b84872dcb5d
Jonathan East <jonathan_east@ymail.com>
2020-05-17 06:00:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,10 @@ class School
   def add_student(name, grade)
     @roster[grade] = []
     @roster[grade] << name
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
     binding.pry
   end
 
